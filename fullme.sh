e9ac0ded15e63eed7b26b060acb6a714b1ea3716
lanbosudo <lanbosudo@gmail.com>
2021-02-28 11:55:40 +0800

    Remove #list sim for higher version of tt++;
    complete fullskill script;
    
    add fullme support;

---
+++ fullme.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#echo param: "$@";
+curl -o .fullme.html -O $@ &&\
+jpgurl=$(cat .fullme.html |grep b2evo_captcha_tmp |cut -d'"' -f 2)
+jpgurl='http://pkuxkx.net/antirobot/'${jpgurl#./}
+echo $jpgurl
+curl -o .fullme.jpg -O $jpgurl &&\
+./lsix
